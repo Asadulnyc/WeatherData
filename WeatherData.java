e5e1860b8b999ce3ab3a8a3cb30a29497959a350
Asadulnyc <asadulk@nycstudents.net>
2024-12-18 16:49:47 +0000

    Finished doc

--- WeatherData.java
+++ WeatherData.java
@@ -1,22 +1,52 @@
-public class WeatherData
-{
+import java.util.ArrayList;
+
+public class WeatherData {
     /** Guaranteed not to be null and to contain only non-null entries */
     private ArrayList<Double> temperatures;
 
-    /**
-    * Cleans the data by removing from temperatures all values that are less than
-    * lower and all values that are greater than upper, as described in part (a)
-    */
-    public void cleanData(double lower, double upper)
-    { /* to be implemented in part (a) */ }
+    public WeatherData(ArrayList<Double> temp) {
+        temperatures = temp;
+    }
 
     /**
-    * Returns the length of the longest heat wave found in temperatures, as described in
-    * part (b)
-    * Precondition: There is at least one heat wave in temperatures based on threshold.
-    */
-    public int longestHeatWave(double threshold)
-    { /* to be implemented in part (b) */ }
-    
-    // There may be instance variables, constructors, and methods that are not shown.
+     * Cleans the data by removing from temperatures all values that are less than
+     * lower and all values that are greater than upper, as described in part (a)
+     */
+    public void cleanData(double lower, double upper) {
+        for (int i = 0; i < temperatures.size(); i++) {
+            double temp = temperatures.get(i);
+            if (temp < lower || temp > upper) {
+                temperatures.remove(i);
+                i--;
+            }
+        }
+        /* to be implemented in part (a) */ }
+
+    /**
+     * Returns the length of the longest heat wave found in temperatures, as
+     * described in
+     * part (b)
+     * Precondition: There is at least one heat wave in temperatures based on
+     * threshold.
+     */
+    public int longestHeatWave(double threshold) {
+        int heatWave = 0;
+        int max = 0;
+        for (double t : temperatures) {
+            if (t > threshold) {
+                heatWave++;
+                if (heatWave > max)
+                    max = heatWave;
+
+            } else
+                heatWave = 0;
+        }
+        return max;
+        /* to be implemented in part (b) */ }
+
+    // There may be instance variables, constructors, and methods that are not
+    // shown.
+    public String toString() {
+        return temperatures.toString();
+    }
 }
